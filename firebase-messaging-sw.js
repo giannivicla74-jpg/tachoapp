2091b37926ea27720d1ace6aefa1813367565f9e
Gianni <giannivicla74@gmail.com>
2026-08-31 01:06:59 +0200

    feat: rimosso 100% sfondo chiaro ai bordi - icona ora completamente blu scuro a tutto schermo

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,8 +1,8 @@
-// Service Worker GC-TachoControl v10.5 Pro - Supporto Notifiche Push & Offline PWA Caching
+// Service Worker GC-TachoControl v10.6 Pro - Supporto Notifiche Push & Offline PWA Caching
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
-const CACHE_NAME = 'tachocontrol-offline-v10.5';
+const CACHE_NAME = 'tachocontrol-offline-v10.6';
 const ASSETS_TO_CACHE = [
     './',
     'index.html',
